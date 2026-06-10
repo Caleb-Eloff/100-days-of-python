2c8cfd98e9c29bb3b0264198587bc9594c24b656
Caleb-Eloff <eloffcj@outlook.com>
2026-06-10 13:52:59 +0200

    Day 02: Complete Tip Calculator

--- day-04/module_utilization.py
+++
@@ -1,6 +0,0 @@
-import module_creation as modcreation
-
-print(modcreation.my_favourite_number)
-
-# This code imports the module named module_creation and assigns it the alias modcreation.
-# Then it prints the value of my_favourite_number from the module_creation module using the alias.
