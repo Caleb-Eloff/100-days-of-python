e9c88556395323ef3a7e3c3fc8652556af235aed
Caleb-Eloff <eloffcj@outlook.com>
2026-06-10 13:58:09 +0200

    Day 3: Complete Treasure Island Conditional

---
+++ day-03/even_or_odd_exercise.py
@@ -0,0 +1,8 @@
+number = int(input("What is the number you want to check? "))
+
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
+# The if statement checks if the number is divisible by 2 
+# (i.e., even).
