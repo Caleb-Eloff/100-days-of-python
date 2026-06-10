5073bb21151d0765aa585d2ddcd73b257a88f0b9
Caleb-Eloff <eloffcj@outlook.com>
2026-06-10 13:58:58 +0200

    Day 4: Complete Rock Paper Scissors Game

---
+++ day-04/module_creation.py
@@ -0,0 +1,5 @@
+my_favourite_number = 3.14
+
+# This line of code creates a variable named my_favourite_number and assigns it the value of 3.14.
+# This variable can be accessed from other modules that import this module, allowing us to use the
+# value of my_favourite_number in those modules.
