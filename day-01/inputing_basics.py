df030624548e54202d2ccc7f1f17ffce902f59ef
Caleb-Eloff <eloffcj@outlook.com>
2026-06-10 13:56:15 +0200

    Day 1: Complete Band Name Generator

---
+++ day-01/inputing_basics.py
@@ -0,0 +1,8 @@
+print("Hello " + input("What is your name?\n") + "!")
+# In this code, we are using the input() function to get input
+# from the user. The text inside the parentheses and quotes is
+# a prompt that will be displayed to the user. The input()
+# function will wait for the user to enter something and then
+# return that input as a string. We are then concatenating
+# the string "Hello ", the user's input, and "!" to create
+# a personalized greeting.
---
+++ day-01/printing_basics.py
@@ -0,0 +1,16 @@
+# Printing is a function that allows us to display information
+# on the screen. In this case, we are printing the string
+# "Hello world!" to the console. The text inside the parentheses
+# and quotes is called a string literal, and it will be
+# displayed exactly as it is written.
+print("Hello world!")
+
+# We can also print multiple lines of text by using the newline
+# character, which is represented by \n. This tells the program
+# to start a new line after the first string is printed.
+print("Hello world!\nHello world!")
+
+# We can also concatenate strings using the + operator. This allows
+# us to combine multiple strings into one. In this case, we are
+# concatenating the strings "Hello", " ", and "world!".
+print("Hello" + " " + "world!")
