074134e600fe161d0a0f085fa2fa95b165168074
Caleb-Eloff <eloffcj@outlook.com>
2026-06-10 13:44:31 +0200

    Day 05: Complete Password Generator

---
+++ day-05/loop_basics.py
@@ -0,0 +1,10 @@
+fruits = ["Apple", "Peach", "Pear"]
+# This is a list of fruits that we will loop through.
+for fruit in fruits:
+    # This is a for loop that iterates through each fruit in the list of fruits.
+    print(fruit)
+    # Prints the name of the fruit.
+    print(fruit + " pie")
+    # Prints the name of the fruit followed by " pie".
+print(fruits)
+# Prints the list of fruits after the loop is done.
