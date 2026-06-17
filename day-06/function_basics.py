0535831d4a05db4619e54ede4e981dbde76ca324
Caleb-Eloff <eloffcj@outlook.com>
2026-06-17 11:37:24 +0200

    Day 6: Complete Functions and While Loop Basics

---
+++ day-06/function_basics.py
@@ -0,0 +1,8 @@
+def my_function():
+# Use the def keyword to define a function, followed by the function name and parentheses.
+# The code block within every function starts with a colon (:) and is indented.
+    print("Hello!")
+    print("Bye!")
+
+my_function()
+# Use the function name followed by parentheses to call the function.
---
+++ day-06/while_loop_basics.py
@@ -0,0 +1,11 @@
+hours = 24
+
+while hours > 0:
+    print(f"You have {hours} hour(s) left.")
+    hours -= 1
+print("Your time is up!")
+
+# While loops are used to repeat a block of code as long as a condition is true. 
+# In this example, the loop continues to run as long as the variable 'hours' is greater than 0. 
+# Each iteration of the loop prints the number of hours left and then decrements the 'hours' variable by 1. 
+# When 'hours' reaches 0, the loop ends and the final message "Your time is up!" is printed.
